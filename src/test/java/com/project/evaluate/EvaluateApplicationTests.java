4a4b3a2e2a87f881e5c673bf2a4f9f6ef0d9a3f8
Levi <3573897471@qq.com>
2023-01-01 11:32:47 +0800

    2023/01/01 更新 * 更新：使用redis作为缓存

--- src/test/java/com/project/evaluate/EvaluateApplicationTests.java
+++ src/test/java/com/project/evaluate/EvaluateApplicationTests.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.project.evaluate.entity.Faculty;
 import com.project.evaluate.mapper.FacultyMapper;
 import com.project.evaluate.util.JwtUtil;
+import com.project.evaluate.util.redis.RedisCache;
 import io.jsonwebtoken.Claims;;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -12,7 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.annotation.Resource;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -83,21 +86,21 @@ class EvaluateApplicationTests {
 
     //    编码格式
     @Value("${file.character-set}")
-    private  String character;
+    private String character;
     //    文件前缀
     @Value("${file.pre-path}")
     private static String prePath;
     //    缓冲区大小阈值 TODO 无法读取到
     @Value("${file.threshold-size")
-    private  String sizeThreshold;
+    private String sizeThreshold;
 
     //    文件分片最大值
     @Value("${file.file-size-max}")
-    private  String fileSizeMax;
+    private String fileSizeMax;
 
     //
     @Value("${file.request-size-max}")
-    private  String requestSizeMax;
+    private String requestSizeMax;
 
     @Test
     void testGetValue() {
@@ -105,6 +108,17 @@ class EvaluateApplicationTests {
         System.out.println(sizeThreshold);
         System.out.println("file-size-max:" + Long.parseLong(fileSizeMax));
         System.out.println("request-size-max:" + Long.parseLong(requestSizeMax));
+    }
+
+    //    测试redis
+    @Resource
+    private RedisCache redisCache;
+
+    @Test
+    public void testRedis() {
+        redisCache.setCacheObject("hello", "你好");
+        String hello = redisCache.getCacheObject("hello");
+        System.out.println(hello);
     }
 }
 
