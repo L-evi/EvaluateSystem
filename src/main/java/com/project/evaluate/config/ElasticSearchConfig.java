8fe1c96d6f451db45af198ac297b9fcd2d4e90ac
Levi <3573897471@qq.com>
2023-03-14 02:13:25 +0800

    新增：ElasticSearch

---
+++ src/main/java/com/project/evaluate/config/ElasticSearchConfig.java
@@ -0,0 +1,26 @@
+package com.project.evaluate.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+    private String scheme;
+
+    private String address;
+
+    private Integer connectTimeout;
+
+    private Integer socketTimeout;
+
+    private Integer tryConnectTimeout;
+
+    private Integer maxConnectCount;
+
+    private Integer maxConnectPerRoute;
+
+//    参考链接：https://blog.csdn.net/u012674854/article/details/106544493?spm=1001.2101.3001.6661.1&utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-106544493-blog-105932047.pc_relevant_multi_platform_whitelistv4&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-106544493-blog-105932047.pc_relevant_multi_platform_whitelistv4&utm_relevant_index=1
+//    https://blog.csdn.net/weixin_39025362/article/details/105932047
+//    https://blog.csdn.net/weixin_39025362/article/details/105360676
+}
